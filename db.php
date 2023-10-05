6b2130391b15a7a9c7148aa7ffc08b836b4ac75e
yanis <bourel.yanis@gmail.com>
2023-10-05 16:32:35 +0200

    GTA

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$db = 'monsite';
-$user = 'votreNomDUtilisateur'; // Remplacez par votre nom d'utilisateur de la base de données
-$pass = 'votreMotDePasse'; // Remplacez par votre mot de passe de la base de données
-
-$dsn = "mysql:host=$host;dbname=$db";
-$pdo = new PDO($dsn, $user, $pass);
-?>
