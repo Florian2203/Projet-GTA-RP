c8773b9c6ea3845b5c2d02302f3a27f78046e320
Ikraw59 <145101838+Ikraw59@users.noreply.github.com>
2023-10-11 18:04:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,52 @@
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GTA V</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
 
-    
+
     <figure id="Logoduserveur">
         <img src="img/pngaaa.com-3547252.png" alt="Logo du serveur">
     </figure>
     <section id="Formutilisateur">
 
-    <figure id="Logonomutilisateur">
-        <img src="img/SVG.png" alt="Logo Nom d'utilisateur">
-    </figure> 
-<form action="traitement_index.php" method="post">
+        <figure id="Logonomutilisateur">
+            <img src="img/SVG.png" alt="Logo Nom d'utilisateur">
+        </figure>
+        <form action="traitement_index.php" method="post">
             <label for="Utilisateur">
-                <input type="text" id="Utilisateur"
-                placeholder="Nom d'utilisateur" name="userName">
+                <input type="text" id="Utilisateur" placeholder="Nom d'utilisateur" name="userName">
             </label>
-            </section>
+    </section>
     <section>
-    <figure id="Logomdp">
-        <img src="img/MDP.png" alt="Icone MDP">
-    </figure>
-  
+        <figure id="Logomdp">
+            <img src="img/MDP.png" alt="Icone MDP">
+        </figure>
+
         <label for="MDP">
-            <input type="text" id="MDP" placeholder="Mot de Passe" name="MDP">
+            <input type="password" id="MDP" placeholder="Mot de Passe" name="MDP">
         </label>
     </section>
-        <br>
-        <button type="submit" id="redirectButton">Connexion</button> <br>
-</form>
-    
-       
+    <br>
+    <button type="submit" id="redirectButton">Connexion</button> <br>
+    </form>
+
+
     <a id="LienDiscord" href="">Lien Discord</a>
     <br>
     <a id="MotdePasseOublié" href="Motdepassseoublie.php">Mot de passe oublié</a>
 
     <footer>
-    <p id="Droits"> ©2023 tous droits réservés </p>
-</footer>
+        <p id="Droits"> ©2023 tous droits réservés </p>
+    </footer>
 
 
 
 </body>
+
 </html>
